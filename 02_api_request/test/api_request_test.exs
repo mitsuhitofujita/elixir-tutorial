893befa66c8e2ebfda972c08a5ff9b336fa01ec5
Mitsuhito Fujita <mitsuhitofujita@gmail.com>
2023-01-08 14:49:27 +0000

    Add http request test using req

--- 02_api_request/test/api_request_test.exs
+++ 02_api_request/test/api_request_test.exs
@@ -1,8 +1,49 @@
 defmodule ApiRequestTest do
   use ExUnit.Case
-  doctest ApiRequest
+  # doctest ApiRequest
 
-  test "greets the world" do
-    assert ApiRequest.hello() == :world
+  setup do
+    bypass = Bypass.open()
+    {:ok, bypass: bypass}
+  end
+
+  test "When http status code is 200 return body", %{bypass: bypass} do
+    res = ~s<{"hello": "world"}>
+
+    Bypass.expect_once(bypass, fn conn ->
+      Plug.Conn.resp(conn, 200, res)
+    end)
+
+    assert ApiRequest.with_gard("http://localhost:#{bypass.port}") == res
+  end
+
+  test "When http status code is 404 return not found message", %{bypass: bypass} do
+    res = ~s<{"hello": "world"}>
+
+    Bypass.expect_once(bypass, fn conn ->
+      Plug.Conn.resp(conn, 404, res)
+    end)
+
+    assert ApiRequest.with_gard("http://localhost:#{bypass.port}") == "not found"
+  end
+
+  test "when http status code is 500 return server error message", %{bypass: bypass} do
+    res = ~s<{"hello": "world"}>
+
+    Bypass.expect_once(bypass, fn conn ->
+      Plug.Conn.resp(conn, 500, res)
+    end)
+
+    assert ApiRequest.with_gard("http://localhost:#{bypass.port}") == "server error"
+  end
+
+  test "when http status code is 400 return server error message", %{bypass: bypass} do
+    res = ~s<{"hello": "world"}>
+
+    Bypass.expect_once(bypass, fn conn ->
+      Plug.Conn.resp(conn, 400, res)
+    end)
+
+    assert ApiRequest.with_gard("http://localhost:#{bypass.port}") == "request error"
   end
 end
