cf05cb9acf1c59aefd883a28e081823831309492
Mitsuhito Fujita <mitsuhitofujita@gmail.com>
2023-01-08 14:48:50 +0000

    Imprement http request

--- 02_api_request/lib/api_request.ex
+++ 02_api_request/lib/api_request.ex
@@ -3,6 +3,15 @@ defmodule ApiRequest do
   Documentation for `ApiRequest`.
   """
 
+  def simple(url \\ "https://wttr.in/?format=j1") do
+    Req.get!(url).body
+  end
+
+  def with_response(url \\ "https://wttr.in/?format=j1") do
+    %Req.Response{status: 400, body: body} = Req.get!(url)
+    body
+  end
+
   @doc """
   Hello world.
 
@@ -12,34 +21,26 @@ defmodule ApiRequest do
       :world
 
   """
-  def hello do
-    # Simple
-    #
-    # Req.get!("https://wttr.in/?format=j1").body
-
-    # Get
-    #
-    # %Req.Response{status: 400, body: body} = Req.get!("https://wttr.in/?format=j1")
-    # body
-
+  def with_gard(url \\ "https://wttr.in/?format=j1") do
     # Return message when error
     #
-    body(Req.get!("https://wttr.in/?format=j1"))
+    Req.get!(url, retry: false)
+    |> body
   end
 
-  def body(%Req.Response{status: 200, body: body}) do
+  defp body(%Req.Response{status: 200, body: body}) do
     body
   end
 
-  def body(%Req.Response{status: 404}) do
+  defp body(%Req.Response{status: 404}) do
     "not found"
   end
 
-  def body(%Req.Response{status: status_code}) when status_code >= 500 do
+  defp body(%Req.Response{status: status_code}) when status_code >= 500 do
     "server error"
   end
 
-  def body(%Req.Response{status: status_code}) when status_code >= 400 do
+  defp body(%Req.Response{status: status_code}) when status_code >= 400 do
     "request error"
   end
 end
